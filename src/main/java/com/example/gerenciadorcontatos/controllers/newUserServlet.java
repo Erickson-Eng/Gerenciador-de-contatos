dca8ffdf5585e68800fe3e54c5d92762675423cd
Erickson Azevedo <erickson.tulio96@gmail.com>
2021-09-10 14:30:22 -0300

    servlet

---
+++ src/main/java/com/example/gerenciadorcontatos/controllers/newUserServlet.java
@@ -0,0 +1,19 @@
+package com.example.gerenciadorcontatos.controllers;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "newUserServlet", value = "/newUser")
+public class newUserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
