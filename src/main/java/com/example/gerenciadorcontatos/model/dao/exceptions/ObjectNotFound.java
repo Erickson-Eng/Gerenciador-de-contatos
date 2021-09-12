dc3dfc5145f90b9c43edcada13e7640c15c09222
Erickson Azevedo <erickson.tulio96@gmail.com>
2021-09-12 09:37:51 -0300

    created search and user authorization

---
+++ src/main/java/com/example/gerenciadorcontatos/model/dao/exceptions/ObjectNotFound.java
@@ -0,0 +1,8 @@
+package com.example.gerenciadorcontatos.model.dao.exceptions;
+
+public class NoResultException extends RuntimeException{
+
+    public NoResultException(String message) {
+        super(message);
+    }
+}
